91c3f69c687a0624bbd16f1fe87b9d7a0b311a14
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-04-08 23:59:04 -0600

    working on it, close

--- SumOfNumbersRecursionO55/Program.cs
+++ SumOfNumbersRecursionO55/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Sum(5));
+                                      
+    }
+
+    static int Sum(int number)
+    {
+        //Exit case, base case (NEEDS AN EXIT CASE on top of it)
+        if (number == 1)
+        {
+            return 1;
+        }
+        else{ //recursive call
+        return number + Sum(number - 1);
+        }
+    //call stack looks ike this 
+    // 5 * Factorial Solver(4)
+    // 5 * 4 * Factorial Solver(3)
+    // 5 * 4 * 3 * Factorial Solver(2)
+    // 5 * 4 * 3 * 2 Factorial Solver(1)
+    // 5 * 4 * 3 * 2 * 1
+    }
+
+}
